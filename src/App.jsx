b792f8d08dfbe621aed42007c7beec2df1fdc581
Nazar <example"gmail.com>
2024-09-11 22:11:01 +0300

    Start page created

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,50 @@
+import { useState, useRef, useEffect } from "react";
+import styles from "./styles/weather-app.module.css";
+
 function App() {
-  return <></>;
+  const [location, setLocation] = useState(null);
+  const inputRef = useRef();
+
+  function successGetLocation(position) {
+    setLocation(position);
+    console.log(position.coords);
+  }
+
+  document.addEventListener("keydown", () => {
+    if (event.key === "Enter") {
+      console.log(inputRef.current.value);
+    }
+  });
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition(
+      successGetLocation
+    );
+  }, []);
+
+  return (
+    <div className={styles.container}>
+      <img
+        className={styles.sun_logo}
+        src="https://img.icons8.com/?size=100&id=8EUmYhfLPTCF&format=png&color=000000"
+        alt="sun"
+      />
+      {location ? (
+        <div>sdfsdf</div>
+      ) : (
+        <div className={styles.prompt}>
+          <h1 className={styles.text}>
+            Allow us to get your location before starting or
+            do it manually
+          </h1>
+          <input
+            type="text"
+            className={styles.input}
+            ref={inputRef}
+          />
+        </div>
+      )}
+    </div>
+  );
 }
 
 export default App;
